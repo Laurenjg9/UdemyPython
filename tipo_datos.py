0228e6dc7da084db9f1c8183e80ab423dc75ac6d
Laurenjg9 <laurenju94@gmail.com>
2025-08-09 14:18:25 -0500

    Repasando coneptos de datos

--- tipo_datos.py
+++ tipo_datos.py
@@ -1,4 +1,4 @@
-"""#Substraccion
+#Substraccion: Me permite substraer elementos de un caracter segun la posicion que elija
 print("Hola"[-1])
 
 #String
@@ -6,9 +6,10 @@ print("123"+"456")
 
 #Entero
 print(123+345)
+print(123_456_3254) #Aun usando guion bajo, lo interpreta como un entero
 
 #Float
-print(3,1416)
+print(3.1416)
 
 #Boolean
 print(True)
@@ -17,17 +18,20 @@ print(False)
 Lista_dict = [{"nombre":"Lauren"},{"nombre":"Valeria"},{"nombre":"Maria"},{"nombre":"Juliana"}]
 print(type(Lista_dict))
 print(Lista_dict[2])
-"""
+
+#lenght
+len("hello")
+len("65468798")
 
 # CastingL
 
-"""print(int("123")+int("234"))
+print(int("123")+int("234"))
 
 print(f"Number fo letters in your name: {len(input("Enter your name: ").strip())}")
 
 Nombre = input("Cual es tu nombre?: ")
 letras = len(Nombre.strip())
 
-print("Numero de letras en tu nombre: " + str(letras))"""
+print("Numero de letras en tu nombre: " + str(letras))
 
 print(3 * (3 + 3) / 3 - 3)
