6ed4017de67b49b9fd712432e8ce5f24ef33b197
Laurenjg9 <laurenju94@gmail.com>
2025-08-10 17:27:56 -0500

    Creacion de Funciones y actualizacion de variables

--- docs/Cod/print_input.py
+++ docs/Cod/print_input.py
@@ -1,12 +1,14 @@
-## Un # comenta una linea
-## El comando ctrl + / tambien comenta una linea
-
+# 1.Ejemplo de uso de print
 print("Hola mundo\n")
 print("Hola mundo\nEstoy aprendiendo Python")
 
-# Probando Variables, input y print junto
-nombre = input("Cual es tu nombre?: ")
-size = len(nombre)
-print(f"Hola {nombre}!\nTu nombre tiene {size} caracteres")
+# 2. Probando Variables, input y print junto
 
-print("Prueba para cambiar los archivos de rama")
+# Solicita el nombre al usuario
+nombre = input("Cual es tu nombre?: ") 
+
+# Calcula la longitud del nombre
+size = len(nombre)  
+
+# Muestra el saludo y la longitud del nombre                   
+print(f"Hola {nombre}!\nTu nombre tiene {size} caracteres") # Muestra el saludo y la longitud del nombre
